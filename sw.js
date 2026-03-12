a2b0ecef30e2494a7a06db6b9a8be1b9ece16d22
butobiz <butobiz@gmail.com>
2026-03-12 15:20:03 +0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'sarangbar-v1.4';
+const CACHE_NAME = 'sarangbar-v1.5';
 const ASSETS = [
   './',
   './index.html',
@@ -28,6 +28,7 @@ self.addEventListener('fetch', (event) => {
     })
   );
 });
+
 
 
 
